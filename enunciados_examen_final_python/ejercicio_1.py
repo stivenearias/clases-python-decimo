2fc84492d72545b5f480a9bc5fe4dc38958abb92
stivenearias <bsea096@gmail.com>
2025-06-10 13:14:37 -0500

    Subir enunciados del examen

---
+++ enunciados_examen_final_python/ejercicio_1.py
@@ -0,0 +1,23 @@
+"""
+Ejercicio 1: Analizador de Texto
+
+Crea un programa que:
+
+1. Pida al usuario ingresar un texto (frase o párrafo).
+
+2. Con funciones, calcule y muestre lo siguiente:
+  - Número de vocales (a, e, i, o, u) en el texto (usar for y condicionales).
+  - Número de palabras (separadas por espacios).
+  - Determinar si el texto es largo o corto:
+    "Corto" si tiene menos de 10 caracteres.
+    "Mediano" si tiene entre 10 y 30 caracteres.
+    "Largo" si tiene más de 30 caracteres (usar len() y condicionales).
+
+3. Imprimir todos los resultados con formatos claros (ej: "Vocales: 5").
+
+Requisitos adicionales:
+  - Usa un bucle para permitir que el usuario analice otro texto 
+  sin salir del programa (hasta que ingrese "salir").
+  - Maneja mayúsculas/minúsculas (ej: contar 'A' como vocal).
+  - Usar funciones para cada tarea requerida
+"""
---
+++ enunciados_examen_final_python/ejercicio_2.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio 2: Diccionario de Contactos
+
+Crea un programa que simule una agenda de contactos usando un diccionario:
+
+1. El programa debe permitir al usuario (una funcion para cada acción):
+  - Agregar un contacto (nombre y teléfono).
+  - Buscar un contacto por nombre.
+  - Mostrar todos los contactos.
+
+2. Usa un bucle while para que el menú se repita hasta que el usuario elija Salir.
+
+3. Implementa funciones separadas para cada opción (agregar, buscar, mostrar).
+
+4. Usa match-case (o if-elif-else) para manejar las opciones del menú.
+"""
